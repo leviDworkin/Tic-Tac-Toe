f65c6c9b5ec1d89abc2becd31d9370e7828122e7
leviDworkin <leviDworkin@gmail.com>
2018-05-17 14:37:34 +0300

    fixed champ

--- Champion.h
+++ Champion.h
@@ -2,10 +2,7 @@
 #include "Player.h"
 
 class Champion: public Player{
-  private:
-    char playingAs;
   public:
-    Champion(char c): playingAs(c) {}
     const string name() const override { return "Levi"; }
   	const Coordinate play(const Board& board) override{
       return {0,0};
